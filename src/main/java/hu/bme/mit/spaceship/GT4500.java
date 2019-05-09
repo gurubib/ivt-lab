12a68a414a166dc5c6686f830020d02c219f7d07
Barnabas Gurubi <gurubibarni@gmail.com>
2019-05-09 20:43:51 +0200

    Adding constructor DI to GT4500

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -14,6 +14,11 @@ public class GT4500 implements SpaceShip {
     this.primaryTorpedoStore = new TorpedoStore(10);
     this.secondaryTorpedoStore = new TorpedoStore(10);
   }
+  
+  public GT4500(TorpedoStore primary, TorpedoStore secondary) {
+	this.primaryTorpedoStore = primary;
+	this.secondaryTorpedoStore = secondary;
+  }
 
   public boolean fireLaser(FiringMode firingMode) {
     // TODO not implemented yet
@@ -78,11 +83,9 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
-	
-	if(!(primaryTorpedoStore.isEmpty() || secondaryTorpedoStore.isEmpty())) {
-		firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
-	}
+		if(!(primaryTorpedoStore.isEmpty() || secondaryTorpedoStore.isEmpty())) {
+			firingSuccess = primaryTorpedoStore.fire(1) && secondaryTorpedoStore.fire(1);
+		}
 
         break;
     }
