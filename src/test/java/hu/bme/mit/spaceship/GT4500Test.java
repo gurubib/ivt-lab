ad7f84a468b5c0ac048ccfd94869bf581dc7df19
Barnabas Gurubi <gurubibarni@gmail.com>
2019-05-09 20:44:40 +0200

    Change the given tests to use mocking

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,47 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore primaryMock;
+  private TorpedoStore secondaryMock;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+	this.primaryMock = mock(TorpedoStore.class);
+	this.secondaryMock = mock(TorpedoStore.class);
+	  
+    this.ship = new GT4500(this.primaryMock, this.secondaryMock);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+	//Mock behaviour
+	when(primaryMock.fire(1)).thenReturn(true);
+	when(secondaryMock.fire(1)).thenReturn(true);
 
     // Act
     boolean result = ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
     assertEquals(true, result);
+	verify(primaryMock, times(1)).fire(1);
+	//verify(secondaryMock, times(1)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
-    // Act
+	//Mock behaviour
+	when(primaryMock.fire(1)).thenReturn(true);
+	when(secondaryMock.fire(1)).thenReturn(true);
+    
+	
+	// Act
     boolean result = ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
     assertEquals(true, result);
+	verify(primaryMock, times(1)).fire(1);
+	verify(secondaryMock, times(1)).fire(1);
   }
-
 }
